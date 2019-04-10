24364aebf50a4ce7d94718cc82763674725c23ff
Joon-S <niceweather94@gmail.com>
2019-04-10 10:08:02 +0900

    Initial commit

---
+++ src/main/java/com/joons/jwt/api/HelloController.java
@@ -0,0 +1,13 @@
+package com.joons.jwt.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello joonseo";
+    }
+}
